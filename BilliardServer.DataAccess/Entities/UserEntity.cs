589516d1767ffb745d8ed9d988f93d0d6c99d0c0
Kirill Borodin <borodin.k.i@appfox.ru>
2026-03-27 15:26:39 +0300

    DB User entity extended

--- BilliardServer.DataAccess/Entities/UserEntity.cs
+++ BilliardServer.DataAccess/Entities/UserEntity.cs
@@ -6,5 +6,15 @@ namespace BilliardServer.Infrastructure.Entities
     {
         public string Name { get; set; } = string.Empty;
         public int Avatar { get; set; }
+
+        public int Exp { get; set; }
+        public int Rating { get; set; }
+
+        public int Chips { get; set; }
+        public int Coins { get; set; }
+
+        public int PartiesCount { get; set; }
+        public int WinPartiesCount { get; set; }
+        public int TotalChipsPrize { get; set; }
     }
 }
---
+++ BilliardServer.DataAccess/Migrations/20260327122110_UserEntityExtended.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BilliardServer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserEntityExtended : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Chips",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Coins",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Exp",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PartiesCount",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalChipsPrize",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WinPartiesCount",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Chips",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Coins",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Exp",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PartiesCount",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TotalChipsPrize",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WinPartiesCount",
+                table: "AspNetUsers");
+        }
+    }
+}
